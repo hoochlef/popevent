855f02141f90d62a64df88166a5bc176fa5d0e2b
hoochlef <amin.hochlef@gmail.com>
2026-04-27 09:26:08 +0100

    Add more tests for the was_created_recently method - Check the behavior with an event that's older than 24hrs - Check the behavior with an event that's recent within the past 24hrs

--- events/tests.py
+++ events/tests.py
@@ -14,3 +14,19 @@ class EventModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_event = Event(created_at=time)
         self.assertIs(future_event.was_created_recently(), False)
+
+    def test_was_created_recently_with_old_event(self):
+        """
+        was_created_recently() returns False for events whose created_at is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_event = Event(created_at=time)
+        self.assertIs(old_event.was_created_recently(), False)
+
+    def test_was_created_recently_with_recent_event(self):
+        """
+        was_created_recently() returns True for events whose created_at is within the past 24hrs.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_event = Event(created_at=time)
+        self.assertIs(recent_event.was_created_recently(), True)
